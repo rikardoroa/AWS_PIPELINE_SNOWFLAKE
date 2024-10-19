498600f24fed6d13850f01c4dd50aada6ba37148
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2024-10-19 17:15:58 -0500

    update repo

--- resource_queries/V0.1.1__stage_creation.sql
+++
@@ -1,7 +0,0 @@
-USE DATABASE {{ database }};
-USE SCHEMA {{ schema }};
-
-CREATE OR REPLACE STAGE fire_incidents_stage
-  URL= '{{ bucket }}'
-  DIRECTORY = ( ENABLE = TRUE,  AUTO_REFRESH = true )
-  STORAGE_INTEGRATION = report_incidents_aws_integration;
