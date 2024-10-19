786cd36881c7e07a7b9c08257cca2577081f96f3
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2024-10-19 13:23:01 -0500

    Update bucket.tf

--- aws_pipeline_deployment/lambda_module/bucket.tf
+++ aws_pipeline_deployment/lambda_module/bucket.tf
@@ -1,3 +1,37 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key" "dts_kms_key" {
+  description             = "Key for encryption"
+  enable_key_rotation     = true
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  
+}
+
+resource "aws_kms_key_policy" "bucket_kms_key" {
+  key_id = aws_kms_key.example.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "key-default-1"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_kms_alias" "dts_kms_alias" {
+  name          = "alias/dt-ft-key"
+  target_key_id = aws_kms_key.dts_kms_key.key_id
+}
+
+
 resource "aws_s3_bucket" "bucket_creation" {
 
    for_each = {
@@ -21,3 +55,16 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   restrict_public_buckets = true
 }
 
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "ss_kms_key" {
+  for_each = aws_s3_bucket.bucket_creation
+
+  bucket = each.value.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.dts_kms_key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
