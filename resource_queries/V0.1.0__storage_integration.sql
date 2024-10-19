ef7b6ad6f13064a3ee264e5c6b67e9f9f98bb901
Ricardo Roa <32416860+rikardoroa@users.noreply.github.com>
2024-10-19 17:00:40 -0500

    Update V0.1.0__storage_integration.sql

--- resource_queries/V0.1.0__storage_integration.sql
+++ resource_queries/V0.1.0__storage_integration.sql
@@ -5,6 +5,6 @@ USE SCHEMA {{ schema }};
 CREATE OR REPLACE STORAGE INTEGRATION report_incidents_aws_integration
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = 'S3'
-  STORAGE_AWS_ROLE_ARN =  {{ role }} 
+  STORAGE_AWS_ROLE_ARN =  '{{ role }}'
   ENABLED = TRUE
-  STORAGE_ALLOWED_LOCATIONS = ({{ bucket }} );
+  STORAGE_ALLOWED_LOCATIONS = ('{{ bucket }}');
